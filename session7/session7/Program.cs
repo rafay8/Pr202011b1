711e110b97cb6af8fcfe13333504e16be0c8f60d
Student <STD@Student_msg.com>
2021-12-16 20:34:30 +0500

    This is my 1st commit

---
+++ session7/session7/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace session7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            students objStudent = new students();
+            Grade objGrade = new Grade();
+            objStudent.PrintDetails();
+            objGrade.PrintDetails();
+}
+        class students
+        {
+            string _studentName = "James";
+            string _address = "California";
+            public virtual void PrintDetails()
+            {
+                Console.WriteLine("Student Name:" + _studentName);
+                Console.WriteLine("Address:" + _address); 
+            }
+        }
+        class Grade : students
+        {
+            string _class = "Four";
+            float _percent = 71.25F;
+            public override void PrintDetails()
+            {
+                Console.WriteLine("Class:" + _class);
+                Console.WriteLine("Percentage:" + _percent);
+            }
+
+
+
+        }
+    }
+}
